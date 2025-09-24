385df9150c8ae030aadb6025fa31e351bbec4b9b
AhmadSubkhi <254107020155@student.polinema.ac.id>
2025-09-24 14:12:52 +0700

    edited by AhmadSubkhi

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test{
     public static void main(String[] args) {
         System.out.println("Hai ini baris kode pertama saya");
         System.out.println("edit by sakti");
+        
+        System.out.println("edit by AhmadSubkhi");
     }
 }
